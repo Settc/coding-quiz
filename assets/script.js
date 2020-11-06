a9ccadf550f7bc609dc494f39157702d8512393d
Thomas Whittle <tewhittle@gmail.com>
2020-11-05 23:30:43 -0500

    Add questions to script file, along with structure to which questions will be appended in HTML file

--- assets/script.js
+++ assets/script.js
@@ -59,7 +59,7 @@
 //It will persistently store your initials and score, and rank them
 //Need two buttons, one to start over, one to clear high scores
 
-var question = document.querySelector(".question");
+var question = document.getElementById("question");
 var answerOne = document.getElementById("answerOne")
 var answerTwo = document.getElementById("answerTwo")
 var answerThree = document.getElementById("answerThree")
@@ -68,10 +68,94 @@ var answerFour = document.getElementById("answerFour")
 var startButton = document.getElementById("startButton")
 var timer = document.getElementById("timer")
 var score = document.getElementById("score")
+var gameOver = document.getElementById("gameOver")
 
 var revealed = document.querySelectorAll(".revealed")
 var highScores = 0
-timer.innerHTML = 60;
+timer.innerHTML = 0;
+
+var questionList = [
+    {
+    question: "A data structure that stores true or false statements is called a ____?",
+        answerOne: "string",
+        answerTwo: "boolean",
+        answerThree: "forEach",
+        answerFour: "array",
+        correctAnswer: answerTwo,
+},
+    {
+    question: "Inside which HTML element do we put the Javascript?",
+        answerOne: "<javascript>",
+        answerTwo: "<scripting>",
+        answerThree: "<script>",
+        answerFour: "<js>",
+        correctAnswer: answerThree,
+},
+    {
+    question: "Which built-in method returns the character at the specified index?",
+        answerOne: "characterAt()",
+        answerTwo: "getCharAt()",
+        answerThree: "charAt()",
+        answerFour: "None of the above",
+        correctAnswer: answerThree,
+},
+    {
+    question: "How to write an IF statement in JavaScript?",
+        answerOne: "if i = 5 then",
+        answerTwo: "if i = 5",
+        answerThree: "if i == 5 then",
+        answerFour: "if (i === 5)",
+        correctAnswer: answerFour,
+},
+    {
+    question: "What is the 'exactly equals to' operator?",
+        answerOne: "=",
+        answerTwo: "==",
+        answerThree: "!=",
+        answerFour: "none of the above",
+        correctAnswer: answerFour,
+},
+    {
+    question: "What keyword exits a function?",
+        answerOne: "return",
+        answerTwo: "exit",
+        answerThree: "escape",
+        answerFour: "none of the above",
+        correctAnswer: answerOne,
+},
+    {
+    question: "JavaScript files have an extension of ___?",
+        answerOne: ".java",
+        answerTwo: ".js",
+        answerThree: ".javascript",
+        answerFour: ".xml",
+        correctAnswer: answerTwo,
+},
+    {
+    question: "isNaN() evaluates an argument to determine if given value is...?",
+        answerOne: "Not a number",
+        answerTwo: "Not a null",
+        answerThree: "Not a new object",
+        answerFour: "None of the above",
+        correctAnswer: answerOne,
+},
+    {
+    question: "Which of the below is used in JavaScript to insert special characters?",
+        answerOne: "&",
+        answerTwo: "-",
+        answerThree: "\\",
+        answerFour: "%",
+        correctAnswer: answerThree,
+},
+    {
+    question: "Code between a set of curly braces is called a ___ of code",
+        answerOne: "Section",
+        answerTwo: "Division",
+        answerThree: "Container",
+        answerFour: "Block",
+        correctAnswer: answerFour,
+},
+]
 
 
 // WHEN I click the start button
@@ -82,13 +166,13 @@ startButton.addEventListener("click", function()
 
     
 {   
-    timer.innerHTML = 60;
+    
 
     for (var i = 0; i < revealed.length; i++) {
         revealed[i].setAttribute("style", "display:none")
     }
 
-    setInterval(updateTimer, 1000);
+    var setTimer = setInterval(updateTimer, 1000);
     var startingTime = 1
     var time = startingTime * 60;
 
@@ -98,13 +182,50 @@ function updateTimer() {
     timer.innerHTML = `${seconds}`;
     time--;
 
-    if (!time) {
-        return
+    if (time < 0) {
+        clearInterval(setTimer);
+        gameOver.innerText = "GAME OVER"
     }
     
     
     
 }
 
+question.textContent = questionList[i].question;
+    answerOne.textContent = questionList[i].answerOne
+    answerTwo.textContent = questionList[i].answerTwo
+    answerThree.textContent = questionList[i].answerThree
+    answerFour.textContent = questionList[i].answerFour
+    
 //and I am presented with a question
 })
+
+
+/*
+<div class="col">
+                <h2>Question goes here</h2>
+            </div>
+<ol class="question">
+                    <li id="answerOne"></li>
+                    <li id="answerTwo"></li>
+                    <li id="answerThree"></li>
+                    <li id="answerFour"></li>
+                </ol>
+*/
+
+
+
+
+var randomInt = function(value) {
+    var questionAmount = Math.floor(Math.random() * value)
+    return questionAmount;
+}
+
+// for (var i = 0; i < questionList.length; i ++) 
+
+function random(choices)
+{
+   
+    
+    
+}
